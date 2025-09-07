d282a3bc3f881273fe44af22d38934f390211b3e
bbaustin <benjaminboydaustin@gmail.com>
2025-09-07 14:31:55 +0900

    Mess around with cards a bit. Need to fix longcat bug

--- src/cardLibrary.ts
+++ src/cardLibrary.ts
@@ -63,9 +63,7 @@ export const CARD_LIBRARY: Card[] = [
     ],
   },
   {
-    text: [
-      'Roll to the left and right one space, ending in your original space',
-    ],
+    text: ['Roll back and forth'],
     cost: [2],
     caloriesBurned: [3],
     effect: [
@@ -117,34 +115,34 @@ export const CARD_LIBRARY: Card[] = [
       },
     ],
   },
-  {
-    text: ['Void warp to a random tile'],
-    cost: [1],
-    caloriesBurned: [0],
-    effect: [
-      // TODO!
-    ],
-    description:
-      'Immediately transport yourself to a random tile. Burn as many calories as you would have moving there physically.',
-  },
-  {
-    text: ['Send a void tendril to snatch an enemy to the right'],
-    cost: [1],
-    caloriesBurned: [0],
-    effect: [
-      // TODO!
-    ],
-    description:
-      'Shoot out a tentacle to the right, until it hits an enemy. That enemy will be eliminated from the grid',
-  },
-  {
-    text: ['Assume offensive nap position'],
-    cost: [3],
-    caloriesBurned: [0],
-    effect: [
-      // TODO!
-    ],
-    description:
-      'Curl into a ball. You only take up one tile (where your head is). You cannot move or burn calories. Pull in and consume all objects or enemies in a square shape around you. Once you are hit, you will enter standard position.',
-  },
+  // {
+  //   text: ['Void warp to a random tile'],
+  //   cost: [1],
+  //   caloriesBurned: [0],
+  //   effect: [
+  //     // TODO!
+  //   ],
+  //   description:
+  //     'Immediately transport yourself to a random tile. Burn as many calories as you would have moving there physically.',
+  // },
+  // {
+  //   text: ['Send a void tendril to snatch an enemy to the right'],
+  //   cost: [1],
+  //   caloriesBurned: [0],
+  //   effect: [
+  //     // TODO!
+  //   ],
+  //   description:
+  //     'Shoot out a tentacle to the right, until it hits an enemy. That enemy will be eliminated from the grid',
+  // },
+  // {
+  //   text: ['Assume offensive nap position'],
+  //   cost: [3],
+  //   caloriesBurned: [0],
+  //   effect: [
+  //     // TODO!
+  //   ],
+  //   description:
+  //     'Curl into a ball. You only take up one tile (where your head is). You cannot move or burn calories. Pull in and consume all objects or enemies in a square shape around you. Once you are hit, you will enter standard position.',
+  // },
 ];
